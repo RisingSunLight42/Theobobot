1194e1e8dd84e8220ddaa91784626444308d1113
RisingSunLight <risingsunlight5776@gmail.com>
2022-05-12 20:44:57 +0200

    Ajout du nettoyage de la chaîne de caractère et de la conversion de celle-ci en array

--- events/message_create.js
+++ events/message_create.js
@@ -7,5 +7,12 @@ module.exports = {
         //* Définit un regex qui permet de match tous les emojis classiques
         const regex_emoji = /\p{Extended_Pictographic}/gu;
         const regex_ponctuation = /\?,;:!'\*\\}{\(\)\[\]-"~\^%\+\//g;
+
+        //* Nettoyage de la chaîne de caractères
+        contenu = await contenu.replaceAll(regex_emoji, "");
+        contenu = await contenu.replaceAll(regex_emoji, "");
+
+        //* Conversion en array
+        contenu = await contenu.split(" ");
     },
 };
