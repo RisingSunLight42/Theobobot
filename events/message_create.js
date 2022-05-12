ce79055e99467b2f8c5f2a4f8808b58e86c4af07
RisingSunLight <risingsunlight5776@gmail.com>
2022-05-12 21:07:27 +0200

    Correction du nettoyage de la chaîne et conversion en array sans les éléments string vides et isolement du dernier mot

--- events/message_create.js
+++ events/message_create.js
@@ -6,13 +6,14 @@ module.exports = {
 
         //* Définit un regex qui permet de match tous les emojis classiques
         const regex_emoji = /\p{Extended_Pictographic}/gu;
-        const regex_ponctuation = /\?,;:!'\*\\}{\(\)\[\]-"~\^%\+\//g;
+        const regex_ponctuation = /[^\w\s]/g; // Remplace tout ce qui n'est pas des nombres, caractères de ponctuations, espaces ou underscore
 
         //* Nettoyage de la chaîne de caractères
         contenu = await contenu.replaceAll(regex_emoji, "");
-        contenu = await contenu.replaceAll(regex_emoji, "");
+        contenu = await contenu.replaceAll(regex_ponctuation, "");
 
-        //* Conversion en array
-        contenu = await contenu.split(" ");
+        //* Conversion en array en retirant tous les éléments vides et enregistre le dernier mot de la chaîne
+        contenu = await contenu.split(" ").filter(String);
+        const mot = contenu[contenu.length - 1];
     },
 };
