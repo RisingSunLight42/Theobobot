3542fbe93f5869044259aa30930a6aaf7e4b9d09
RisingSunLight <risingsunlight5776@gmail.com>
2022-05-12 20:32:39 +0200

    Modification des noms de variables et ajout du regex pour la ponctuation

--- events/message_create.js
+++ events/message_create.js
@@ -2,9 +2,10 @@ module.exports = {
     name: "messageCreate",
     async execute(message) {
         //* Stockage du contenu du message
-        let message = message.content;
+        let contenu = message.content;
 
         //* Définit un regex qui permet de match tous les emojis classiques
-        const regex = /\p{Extended_Pictographic}/gu;
+        const regex_emoji = /\p{Extended_Pictographic}/gu;
+        const regex_ponctuation = /\?,;:!'\*\\}{\(\)\[\]-"~\^%\+\//g;
     },
 };
