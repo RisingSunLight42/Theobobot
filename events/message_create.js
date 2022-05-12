e4143837b71b9b94959b59d7446e424fad810fa0
RisingSunLight <risingsunlight5776@gmail.com>
2022-05-12 21:18:03 +0200

    Ajout de la vérification si le mot est assez long, puis création du regex quoi et check s'il est bien présent à la fin du mot pour dire feur

--- events/message_create.js
+++ events/message_create.js
@@ -15,5 +15,12 @@ module.exports = {
         //* Conversion en array en retirant tous les éléments vides et enregistre le dernier mot de la chaîne
         contenu = await contenu.split(" ").filter(String);
         const mot = contenu[contenu.length - 1];
+
+        if (mot.length < 4) return; // Si la longueur du mot est plus petite que 4, ça ne peut pas être quoi, on renvoie
+
+        const regex_quoi = /quoi/y;
+        regex_quoi.lastIndex = mot.length - 4;
+
+        if (regex_quoi.test(mot)) return await message.channel.send("feur !");
     },
 };
