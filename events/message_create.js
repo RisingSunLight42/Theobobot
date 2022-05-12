e9ea4651c719e0b03fbf3a9822a13bf06c45ab7c
RisingSunLight <risingsunlight5776@gmail.com>
2022-05-12 18:45:37 +0200

    Nettoyage des fichiers events

--- events/message_create.js
+++ events/message_create.js
@@ -1,15 +1,6 @@
-const { blacklist_check } = require("../helpers/functions/blacklist_check.js");
-const { react_image } = require("../helpers/functions/react_image.js");
-
 module.exports = {
     name: "messageCreate",
     async execute(message) {
-        if (message.content.toLowerCase().includes("boom the rock")) {
-            await message.channel.send(
-                "https://cdn.discordapp.com/attachments/643529375941132289/969371184649084968/VID_20220328225943.mp4"
-            );
-        }
-        await blacklist_check(message);
-        await react_image(message);
+        // gestion des messages
     },
 };
--- events/ready.js
+++ events/ready.js
@@ -1,49 +1,7 @@
-const {
-    deployGlobal,
-    deployOkora,
-    deployGestion,
-    recupFichier,
-} = require("../deploy-commands.js"); // Importe la fonction pour déployer les commandes
-const {
-    changement_statut,
-} = require("../helpers/functions/changement_statut.js");
-const { souhaite_anniv } = require("../helpers/functions/souhaite_anniv.js");
-const { sequelize } = require("../database/database_gestion.js"); // Import d'Etatcommission
-const { guildIdGestion, guildIdOkora } = require("../config.json");
-
 module.exports = {
     name: "ready",
     once: true,
     execute(client) {
         console.log(`🟢 Je suis allumée !`);
-        //* Synchronise les tables
-        sequelize.sync();
-        console.log("📋 Les tables ont été synchronisées !");
-
-        //* Lance le changement de statut
-        changement_statut(client);
-        setInterval(changement_statut, 1800000, client);
-
-        //* Lance la fonction pour souhaiter l'anniversaire des membres
-        souhaite_anniv(client);
-        setInterval(souhaite_anniv, 86400000, client);
-
-        //* Push les commandes suivant si les serveurs recherchés sont présents et si c'est le bot principal
-        const liste_commandes = recupFichier(client.user.username);
-        client.guilds.fetch().then(function (result) {
-            const guild_liste_snowflake = result.map((objet) => objet.id); // Récupère les ids de guild du bot dans une liste
-            if (guild_liste_snowflake.includes(guildIdGestion)) {
-                // S'il y a le serveur de gestion, push les commandes de gestion
-                deployGestion(liste_commandes[0]);
-            }
-            if (guild_liste_snowflake.includes(guildIdOkora)) {
-                // S'il y a le serveur d'Okora, push les commandes de gestion
-                deployOkora(liste_commandes[1]);
-            }
-        });
-        if (client.user.username === "Tsakali") {
-            // Si c'est le bot officiel, push les commandes restantes en globales
-            deployGlobal(liste_commandes[2]);
-        }
     },
 };
